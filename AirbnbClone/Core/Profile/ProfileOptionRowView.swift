8a8419fd77c5b1240c709dd6757360b7869b4404
drestwn <andremakmursetaiwan@gmail.com>
2025-07-27 10:23:30 +0700

    tabar settings, wishlist page, profile page

---
+++ AirbnbClone/Core/Profile/ProfileOptionRowView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileOptionRowView.swift
+//  AirbnbClone
+//
+//  Created by Andre on 27/07/25.
+//
+
+import SwiftUI
+
+struct ProfileOptionRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileOptionRowView()
+}
